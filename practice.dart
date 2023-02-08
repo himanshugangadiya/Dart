67187425e890da4a8c27a8f5262a17d3bd6d668e
himanshu gangadiya <123546076+himanshugangadiya@users.noreply.github.com>
2023-02-08 09:53:13 +0530

    Update practice.dart

--- practice.dart
+++ practice.dart
@@ -1,9 +1,4 @@
 import 'dart:io';
 
-Future<void> main() async {
-  var file = File('desktop/token.txt');
-  var stream = file.openRead();
-  await for (var data in stream) {
-    print(data.length);
-  }
+
 }
